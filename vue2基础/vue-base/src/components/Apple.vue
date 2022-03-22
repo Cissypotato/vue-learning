6d7055bd4232786cce09086cdef0869fde0e384d
Cissy <1395422644@qq.com>
2022-03-22 16:51:50 +0800

    全局总线事件

---
+++ vue2基础/vue-base/src/components/Apple.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>I am apple</h1>
+        <div>
+            <button @click="handleData">emit data</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Apple",
+    data() {
+        return {
+            
+        }
+    },
+    methods:{
+        handleData(){
+            this.$bus.$emit('receive',666)
+        }
+    }
+}
+</script>
---
+++ vue2基础/vue-base/全局总线事件/components/Peach.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div>
+    <h1>I am Peach</h1>
+    <div>receive data:{{fromApple}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Peach",
+  data() {
+    return {
+        fromApple:undefined
+    };
+  },
+  mounted() {
+    this.$bus.$on("receive", (data) => {
+      console.log("I am Peach", data);
+      this.fromApple=data
+    });
+  },
+  methods: {
+  },
+};
+</script>
