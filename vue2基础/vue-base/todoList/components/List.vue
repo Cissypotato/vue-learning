ddca7488fd992d60f12ef4ec9a7f66586406dbee
Cissy <1395422644@qq.com>
2022-03-22 21:39:02 +0800

    buid：todoList

---
+++ vue2基础/vue-base/todoList/components/List.vue
@@ -0,0 +1,39 @@
+<template>
+  <ul class="todo-main">
+    <list-item
+      v-for="todo in todos"
+      :todo="todo"
+      :key="todo.id"
+      :todoCheck="todoCheck"
+      :removeTodo="removeTodo"
+    />
+  </ul>
+</template>
+<script>
+import ListItem from "./ListItem.vue";
+export default {
+  name: "List",
+  props: ["todos", "todoCheck","removeTodo"],
+  components: {
+    ListItem,
+  },
+};
+</script>
+<style >
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
---
+++ vue2基础/vue-base/todoList/components/MyFooter.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="todo-footer" v-show="todos.length>0">
+    <label>
+      <input type="checkbox" v-model="total" />
+    </label>
+    <span>
+      <span>已完成{{ todoDone }}</span> / 全部{{ todos.length }}
+    </span>
+    <button class="btn btn-danger" @click="clear1">清除已完成任务</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "MyFooter",
+  props: ["todos", "handleAll",'clearDone'],
+  computed: {
+    total: {
+      get() {
+        return this.todoDone === this.todos.length && this.todos.length > 0;
+      },
+      set(value) {
+        this.handleAll(value);
+      },
+    },
+    todoDone() {
+      return this.todos.reduce((pre, cur) => pre + (cur.done ? 1 : 0), 0);
+    },
+   
+  },
+  methods: {
+     clear1(){
+      this.clearDone()
+    }
+  },
+};
+</script>
+<style >
+/*footer*/
+.todo-footer {
+  height: 40px;
+  line-height: 40px;
+  padding-left: 6px;
+  margin-top: 5px;
+}
+
+.todo-footer label {
+  display: inline-block;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.todo-footer label input {
+  position: relative;
+  top: -1px;
+  vertical-align: middle;
+  margin-right: 5px;
+}
+
+.todo-footer button {
+  float: right;
+  margin-top: 5px;
+}
+</style>
