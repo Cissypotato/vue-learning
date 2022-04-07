7f8d83091a3f45f363830c697001c22d9ba4b5b0
Cissy <1395422644@qq.com>
2022-04-07 15:05:18 +0800

    feat:slot

---
+++ vue2基础/vue-base/src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="slot-wrapper">
+        <h1>{{title}}</h1>
+        <slot :fruits="list"></slot><!-- 作用域插槽也可以有名字-->
+    </div>
+</template>
+<script>
+export default {
+    name:'Card',
+    props:['title'],
+    data() {
+    return {
+      list:['durian','mangosteem','mango']
+    }
+  },
+    
+    
+}
+</script>
+<style scoped>
+.slot-wrapper{
+    width: 400px;
+    height: 500px;
+    border: 1px solid #ddd;
+}
+</style>
---
+++ vue2基础/vue-base/插槽/默认插槽/App.vue
@@ -0,0 +1,26 @@
+<template>
+ 
+    <div class="container">
+    <card :title="'默认插槽'">
+     <div> hello slot</div><!--内容要包在标签里面-->
+     <div> hello world</div><!--可以有多个标签-->
+    </card>
+    </div>
+ 
+</template>
+
+<script>
+import Card from './components/Card.vue';
+
+
+export default {
+  name: "App",
+  components: {
+    Card
+    
+  },
+};
+</script>
+
+<style>
+</style>
